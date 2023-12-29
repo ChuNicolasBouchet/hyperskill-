081796adef6eb17d110dea87d16257666c61f339
nico <chunico@pm.me>
2023-12-29 19:58:33 +0100

    stage 4/7 done

--- task/src/bullscows/Main.java
+++ task/src/bullscows/Main.java
@@ -6,54 +6,60 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static String secretCode = "9305";
-    //static String userCode = userInput();
+    static String secretCode = "";
+    static int secretDigits = 0;
     static int countBulls = 0;
     static int countCows = 0;
 
     public static void main(String[] args) {
-        //checkCode(secretCode,userCode);
-        //printAnswer(countBulls,countCows,secretCode);
-        System.out.printf("The random secret number is %d.", generateSecret());
+        secretCode = generateSecret();
+        System.out.println("Okay, let's start a game!");
+        int turnCounter = 1;
+//        System.out.println("The secret is: " + secretCode);
+        while (countBulls != secretDigits) {
+            resetBulls();
+            resetCows();
+            System.out.printf("Turn %d:%n", turnCounter);
+            checkCode(secretCode,userInput());
+            turnCounter++;
+        }
+        System.out.println("Congratulations! You guessed the secret code.");
     }
 
 
-    protected static int generateSecret (){
+    protected static String generateSecret (){
+        System.out.println("Please, enter the secret code's length:");
         Scanner scanner = new Scanner(System.in);
-        int numDigit = 0;
-        while (numDigit <=0 || numDigit > 10) {
-            numDigit = scanner.nextInt();
-            if (numDigit > 10) {
-                System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.%n", numDigit);
+
+        while (secretDigits <=0 || secretDigits > 10) {
+            secretDigits = scanner.nextInt();
+            if (secretDigits > 10) {
+                System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.%n", secretDigits);
             }
         }
-
-        int min = (int) Math.pow(10, numDigit -1); // 10ee digit -1
-        int max = (int) Math.pow(10, numDigit) -1;
+        int min = (int) Math.pow(10, secretDigits -1); // 10ee digit -1
+        int max = (int) Math.pow(10, secretDigits) -1;
         int generated;
         do {
             long pseudoRandomNumber = System.nanoTime();
             Random random = new Random(pseudoRandomNumber);
             generated = random.nextInt(max - min + 1) + min;
-        } while (!areDigitsUnique(generated, numDigit));
-        return generated;
+        } while (!areDigitsUnique(generated));
+        return String.valueOf(generated);
     }
 
-    public static boolean areDigitsUnique(int number, int length) {
-        boolean[] digits = new boolean[10]; // for digits 0 to 9, all initialized at false
-        //loop from the end to the beginning of the number
+    public static boolean areDigitsUnique(int number) {
+        boolean[] digits = new boolean[10];
         while (number > 0) {
-            int digit = number % 10; //extraction of the last digit
-            //check if the digit is already at true in the array
+            int digit = number % 10;
             if (digits[digit]){
-                return false; // if already at true -> not unique -> return false
+                return false;
             }
-            digits[digit] = true; // if not found switch the presence of that digit to true
-            number /= 10; // cut the last digit by dividing by 10
+            digits[digit] = true;
+            number /= 10;
         }
         return true;
     }
-
 
     protected static void checkCode (String secretCode, String userCode) {
         for (int i = 0; i < secretCode.length(); i++){
@@ -65,37 +71,61 @@ public class Main {
                 addCows();
             }
         }
+        printAnswer(countBulls,countCows);
     }
 
-    protected static void printAnswer(int countBull, int countCow, String secretCode) {
-        if (countCow > 0 && countBull == 0){
-            System.out.printf("Grade: %d cow(s). The secret code is %s." ,countCow, secretCode);
-        } else if (countCow == 0 && countBull > 0) {
-            System.out.printf("Grade: %d bull(s). The secret code is %s.", countBull, secretCode);
-        } else if (countCow > 0 && countBull > 0) {
-            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.", countBull,countCow,secretCode);
+    protected static void printAnswer(int bull, int cow) {
+
+        String cowS = "";
+        String bullS = "";
+        if (cow > 1){
+            cowS = "s";
+        }
+        if (bull > 1){
+            bullS = "s";
+        }
+        if (cow > 0 && bull == 0){
+            System.out.printf("Grade: %d cow%s.%n" ,cow, cowS);
+        } else if (cow == 0 && bull > 0) {
+            System.out.printf("Grade: %d bull%s.%n", bull, bullS);
+        } else if (cow > 0 && bull > 0) {
+            System.out.printf("Grade: %d bull%s and %d cow%S.%n", bull, bullS, cow, cowS);
         } else {
-            System.out.printf("Grade: None. The secret code is %s", secretCode);
+            System.out.printf("Grade: None.%n");
         }
     }
     protected static String userInput(){
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
-        while (!isValidInput(userInput)) {
+        while (!isValidInput(userInput, secretDigits)) {
             userInput = scanner.nextLine();
         }
         return userInput;
     }
-    protected static boolean isValidInput(String input){
-        return input.matches("\\d\\d\\d\\d");
-    }
 
+    protected static boolean isValidInput(String userInput, int secretDigits){
+        if (userInput == null || userInput.length() != secretDigits ){
+            return false;
+        }
+        for (int i = 0; i < secretDigits; i++){
+            if (!Character.isDigit(userInput.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
 
     static void addBulls() {
         countBulls++;
     }
+    static void resetBulls(){
+        countBulls = 0;
+    }
     static void addCows() {
         countCows++;
+    }
+    static void resetCows(){
+        countCows = 0;
     }
     public static boolean isLengthValid (String length){
         return length.matches("\\d{1,10}");
